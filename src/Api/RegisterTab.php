b0bd9a64d815830ac1898c30e6217f5b7ccf1916
SobolevAnatoly <sobolev.anatoly@gmail.com>
2021-02-04 19:42:43 +0200

    Formated

--- src/Api/RegisterTab.php
+++ src/Api/RegisterTab.php
@@ -41,11 +41,11 @@ class RegisterTab
         add_filter('woocommerce_product_data_tabs', [$instance, 'regProductDiscountTab']);
         add_filter('woocommerce_product_data_panels', [$instance, 'discountTabContent']);
         add_action('woocommerce_process_product_meta', [$instance, 'saveTabContent']);
-
     }
 
     /**
      * @param $tabs
+     *
      * @return mixed
      * Method hooked to a woocommerce_product_data_tabs filter action.
      * Add the new tab to the $tabs array.
@@ -54,9 +54,9 @@ class RegisterTab
     public static function regProductDiscountTab($tabs)
     {
         $tabs['discounts_tab'] = [
-            'label' => __('Role discounts', 'advanced-discounts'),
-            'target' => 'discounts_product_data',
-            'class' => [],
+            'label'    => __('Role discounts', 'advanced-discounts'),
+            'target'   => 'discounts_product_data',
+            'class'    => [],
             'priority' => 9999
         ];
 
@@ -74,26 +74,32 @@ class RegisterTab
         ?>
         <div id="discounts_product_data" class="panel woocommerce_options_panel hidden">
             <div class="options_group">
-                <h1><?php _e('There you can set discount for different users role.', 'advanced-discounts'); ?></h1>
-                <p><?php _e('Discount calculate in % and must be typed as an integer from 0 to 100. If the field is left blank, will be displayed the price that specified in the General tab', 'advanced-discounts'); ?></p>
+                <h1><?php
+                    _e('There you can set discount for different users role.', 'advanced-discounts'); ?></h1>
+                <p><?php
+                    _e(
+                        'Discount calculate in % and must be typed as an integer from 0 to 100. If the field is left blank, will be displayed the price that specified in the General tab',
+                        'advanced-discounts'
+                    ); ?></p>
                 <?php
                 foreach ($wp_roles->get_names() as $role) {
-
                     $role_code = strtolower(preg_replace('/\s+/', '_', $role));
-                    woocommerce_wp_text_input([
-                        'id' => '_' . $role_code . '_discount_input',
-                        'label' => __('Discount for ' . $role, 'advanced-discounts'),
-                        'wrapper_class' => 'discount_input',
-                        'type' => 'number',
-                        'custom_attributes' => [
-                            'step' => '1',
-                            'min' => '0',
-                            'max' => '100',
-                            'autocomplete' => 'off',
-                            'size' => '3',
-                            'pattern' => '[0-9]{,3}'
+                    woocommerce_wp_text_input(
+                        [
+                            'id'                => '_' . $role_code . '_discount_input',
+                            'label'             => __('Discount for ' . $role, 'advanced-discounts'),
+                            'wrapper_class'     => 'discount_input',
+                            'type'              => 'number',
+                            'custom_attributes' => [
+                                'step'         => '1',
+                                'min'          => '0',
+                                'max'          => '100',
+                                'autocomplete' => 'off',
+                                'size'         => '3',
+                                'pattern'      => '[0-9]{,3}'
+                            ]
                         ]
-                    ]);
+                    );
                 }
                 ?>
             </div>
@@ -108,12 +114,15 @@ class RegisterTab
     public function saveTabContent($post_id)
     {
         $wp_roles = new WP_Roles();
-        $product = wc_get_product($post_id);
+        $product  = wc_get_product($post_id);
 
         foreach ($wp_roles->get_names() as $role) {
-
-            $role = strtolower(preg_replace('/\s+/', '_', $role));
-            $discount_amount = filter_input(INPUT_POST, '_' . $role . '_discount_input', FILTER_SANITIZE_NUMBER_INT) ?? '0';
+            $role            = strtolower(preg_replace('/\s+/', '_', $role));
+            $discount_amount = filter_input(
+                                   INPUT_POST,
+                                   '_' . $role . '_discount_input',
+                                   FILTER_SANITIZE_NUMBER_INT
+                               ) ?? '0';
             $product->update_meta_data('_' . $role . '_discount_input', $discount_amount);
         }
 
